b047006d16a0fd6f9666965e893097918431d2a2
Arshh26 <arshadali99936@gmail.com>
2025-09-17 19:22:13 +0530

    Added Challenges

--- Challenges/PrimeNumberChecker.java
+++ Challenges/PrimeNumberChecker.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class PrimeNumberChecker {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Prime Number Checker\n");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, Enter the number: ");
+        int num = input.nextInt();
+        boolean isPrime = isPrime(num);
+        if(isPrime) {
+            System.out.println("Your Number is Prime");
+        } else {
+            System.out.println("Your Number is not Prime");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        int i = 2;
+        while(i < num){
+            if(num % i == 0){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+
 }
