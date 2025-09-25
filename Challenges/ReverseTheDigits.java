4e87dc0e6ee466f3967230a6d5c87cc8d8cd2668
Arshh26 <arshadali99936@gmail.com>
2025-09-25 18:03:42 +0530

    Added Challenges

--- Challenges/ReverseTheDigits.java
+++ Challenges/ReverseTheDigits.java
@@ -1,21 +1,23 @@
-//import java.util.Scanner;
-//
-//public class ReverseTheDigits {
-//    public static void main(String[] args) {
-//        System.out.println("Welcome to reversing the number\n");
-//        Scanner input = new Scanner(System.in);
-//        System.out.println("Please, Enter Your Number: ");
-//        int num = input.nextInt();
-//        int div = reversTheNumber(num);j
-//        System.out.println("Your reversed Number is: "+ div);
-//    }
-//
-//    public static int reversTheNumber(int num) {
-//        int i = 0;
-//        while(i <= num) {
-//           int div /= 10;
-//            return div;
-//        }
-//        i++;
-//    }
-//}
+import java.util.Scanner;
+
+public class ReverseTheDigits {
+
+    public static void main (String[] args) {
+        System.out.println("Welcome to revering the digit");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, Enter Your Number: ");
+        int num = input.nextInt();
+        int reverse = reverse(num);
+        System.out.println("Your reversed number is: "+ reverse);
+    }
+
+    public static int reverse (int num) {
+        int newNum = 0;
+        while(num > 0) {
+            int digit = num % 10;
+            newNum = newNum * 10 + digit;
+            num /= 10;
+        }
+        return newNum;
+    }
+}
