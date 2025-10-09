2ebc187d2cdd84ac5c269e33202c6e25d6552f83
Arshh26 <arshadali99936@gmail.com>
2025-10-09 19:27:22 +0530

    Updated

---
+++ PracticeSet/ArrayLoop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        System.out.println("Array with loop");
+        int[] number = {2, 4, 6, 8, 10};
+        int i = 0;
+        while(i < number.length){
+            System.out.println(number[i]);
+            i++;
+        }
+
+
+    }
+}
