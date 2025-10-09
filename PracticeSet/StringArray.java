324b7a4ad19bfe174160ac2c0b3d74c59d0e9b30
Arshh26 <arshadali99936@gmail.com>
2025-10-09 21:39:21 +0530

    Updated

---
+++ PracticeSet/StringArray.java
@@ -0,0 +1,24 @@
+public class StringArray {
+    public static void main(String[] args) {
+        System.out.println("String Array\n");
+
+        String[] newStr = new String[4];
+        newStr[0] = "First";
+        newStr[1] = "Second";
+        newStr[2] = "Third";
+        newStr[3] = "Fourth";
+
+
+        System.out.println(newStr[2]);
+//        System.out.println(newStr[0]);
+//        System.out.println(newStr[3]);
+//        System.out.println(newStr[1]);
+
+        String[] myString = {"Arshad", "Abrar", "hafiz", "Nouman" };
+        System.out.println(myString.length);
+
+
+
+
+    }
+}
