993592d8bda837f96bf506d945178ba175af1579
Arshh26 <arshadali99936@gmail.com>
2025-10-10 21:27:09 +0530

    Updated

---
+++ Challenges/ArraySumAverage.java
@@ -0,0 +1,25 @@
+public class ArraySumAverage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Sum and Average\n");
+        int[] numArray = ArrayUtility.inputArray();
+        long sum = sum(numArray);
+        long average = average(numArray);
+        System.out.println("The total sum of an array is: "+sum);
+        System.out.println("The total average of sum is: "+average);
+    }
+
+    public static long sum(int[] numArray){
+        long sum = 0;
+        int i = 0;
+        while(i < numArray.length){
+            sum += numArray[i];
+            i++;
+        }
+        return sum;
+    }
+
+    public static long average (int[] numArray){
+        long sum = sum(numArray);
+        return (sum / numArray.length);
+    }
+}
