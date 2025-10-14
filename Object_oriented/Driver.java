4a75d8ce854abdcf62a50750cd0a2822af63c04e
Arshh26 <arshadali99936@gmail.com>
2025-10-14 21:03:58 +0530

    Updated

--- Object_oriented/Driver.java
+++ Object_oriented/Driver.java
@@ -1,20 +1,20 @@
 public class Driver {
     public static void main(String[] args) {
-//        Car myCar = new Car();
-//        myCar.addFuel(6);
-//        myCar.drive();
-//        myCar.drive();
-//        myCar.drive();
-//        myCar.addFuel(6);
-//        myCar.drive();
-//        myCar.drive();
-//        myCar.drive();
-//        myCar.drive();
-//        myCar.drive();
-//        myCar.drive();
-//        myCar.drive();
-//        myCar.addFuel(3);
-//        System.out.println(myCar.getCurrentFuelLevel());
+        Car myCar = new Car();
+        myCar.addFuel(6);
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.addFuel(6);
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.addFuel(3);
+        System.out.println(myCar.getCurrentFuelLevel());
 
         Car swift = new Car();
         swift.addFuel(6);
