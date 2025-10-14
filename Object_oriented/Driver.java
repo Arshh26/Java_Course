b2fa56730b5a19d3b49c7d9a4f66f06b11d19990
Arshh26 <arshadali99936@gmail.com>
2025-10-14 19:41:36 +0530

    Learning Class vs Object

---
+++ Object_oriented/Driver.java
@@ -0,0 +1,19 @@
+public class Driver {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.addFuel(6);
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.addFuel(6);
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.drive();
+        myCar.addFuel(3);
+        System.out.println(myCar.getCurrentFuelLevel());
+    }
+}
