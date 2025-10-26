f9e8e30203ab4c2d308502df5b7d86fc4cdc8172
Arshh26 <arshadali99936@gmail.com>
2025-10-26 19:46:40 +0530

    Updated

---
+++ Challenges/AbsoluteTernary.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class AbsoluteTernary { //Absolute value makes the nagetive value to positive, and positive value keeps positive
+    public static void main(String[] args) {
+        System.out.println("Welcome for finding absolute vslue \n");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please, Enter your Number: ");
+        int num = input.nextInt();
+        AbsoluteTernary ternary = new AbsoluteTernary();
+        int result = ternary.absoluteValue(num);
+        System.out.println("Your Absolute Value is: "+ result);
+
+
+    }
+    public int absoluteValue(int num){
+        return (num >= 0) ? num : -num;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
