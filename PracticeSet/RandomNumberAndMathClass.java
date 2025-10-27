8c6ae07b5b47366bee76a9750662f1da744e75ac
Arshh26 <arshadali99936@gmail.com>
2025-10-27 22:09:37 +0530

    Updated

---
+++ PracticeSet/RandomNumberAndMathClass.java
@@ -0,0 +1,11 @@
+public class RandomNumberAndMathClass {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-99));
+        System.out.println(Math.ceil(5.59));
+        System.out.println(Math.floor(5.49));
+        System.out.println(Math.round(6.02));
+        System.out.println(Math.random());
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+    }
+}
