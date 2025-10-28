8a5c6b0b7354731921b94e4f1b944083ea246271
Arshh26 <arshadali99936@gmail.com>
2025-10-28 10:07:13 +0530

    Updated

---
+++ PracticeSet/FinalKeyword.java
@@ -0,0 +1,7 @@
+public class FinalKeyword {
+    final double PI = 3.14; //declared final
+
+    void piValue () {
+//        PI = 3.140; // Can't be reassigned because PI value is declared as a final.
+    }
+}
---
+++ PracticeSet/StringTest.java
@@ -0,0 +1,15 @@
+public class StringTest {
+    public static void main(String[] args) {
+        System.out.println();
+        StringBuilder sb = new StringBuilder("My name");
+        sb.append(" is Arshad Ali,");
+        sb.append(" I am from India.");
+        sb.append(" Currentrly i am studying in Visvesvaray technological University, Belagavi.");
+        sb.append(" This is a just for a example.");
+        sb.append(" ThankYou.");
+        System.out.println(sb);
+
+
+
+    }
+}
