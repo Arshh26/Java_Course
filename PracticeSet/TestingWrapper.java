4c6be9edc23d1bc9d8d140997dd713e64fb89f2e
Arshh26 <arshadali99936@gmail.com>
2025-11-07 09:35:05 +0530

    Updated

---
+++ PracticeSet/TestingWrapper.java
@@ -0,0 +1,9 @@
+public class TestingWrapper {
+    public static void main(String[] args) {
+        Integer first = Integer.valueOf(55);
+//        Integer first = 55; (Automatic Conversion)
+        System.out.println(first.floatValue());
+
+
+    }
+}
