306e84fc3a0cbdb362609e7226b07744175d440b
Arshh26 <arshadali99936@gmail.com>
2025-11-09 10:32:03 +0530

    Updated

---
+++ Challenges/UniqueCharactors.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class UniqueCharactors {
+    public static void main(String[] args) {
+        Set<Character> unique = new HashSet<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your String");
+        String userStr = input.next();
+        for (char ch : userStr.toCharArray()) {
+            unique.add(ch);
+        }
+
+        System.out.printf("Your string has %d unique characters", unique.size());
+    }
+
+}
