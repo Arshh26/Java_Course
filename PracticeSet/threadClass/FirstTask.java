3575e7b060d3a961b90d5501f55df86ed75ca7d1
Arshh26 <arshadali99936@gmail.com>
2025-11-09 22:05:39 +0530

    Updated

---
+++ PracticeSet/threadClass/FirstTask.java
@@ -0,0 +1,16 @@
+package threadClass;
+
+
+public class FirstTask extends Thread {
+    @Override
+    public void run() {
+        // First task
+            for (int i = 1; i <= 1000; i++) {
+                System.out.printf("%d* ", i);
+            }
+            System.out.printf("\n %s * task complete",
+                    Thread.currentThread().getName());
+        }
+    }
+
+
