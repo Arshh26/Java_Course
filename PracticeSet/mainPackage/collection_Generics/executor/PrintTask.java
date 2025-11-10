8d9fd79bf703a8141aa5667f1388e9c1f26f3ba4
Arshh26 <arshadali99936@gmail.com>
2025-11-10 10:00:34 +0530

    Updated

---
+++ PracticeSet/mainPackage/collection_Generics/executor/PrintTask.java
@@ -0,0 +1,26 @@
+package mainPackage.collection_Generics.executor;
+
+public class PrintTask implements Runnable{
+        @Override
+        public void run() {
+            // Print task
+            for (int i = 1; i <= 1000; i++) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.printf("%d%c ", i, targetChar);
+            }
+            System.out.printf("\n %s %c task complete",
+                    Thread.currentThread().getName(),
+                    targetChar);
+        }
+
+        private final char targetChar;
+
+        public PrintTask(char targetChar) {
+            this.targetChar = targetChar;
+        }
+    }
+
