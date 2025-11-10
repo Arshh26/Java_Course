334d2388a589ca19c0a3062dd3f4ea491f72427a
Arshh26 <arshadali99936@gmail.com>
2025-11-10 07:37:18 +0530

    Updated

---
+++ Challenges/threadChallenges/ThreadStateTesting.java
@@ -0,0 +1,13 @@
+package threadChallenges;
+
+public class ThreadStateTesting {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadState t1 = new ThreadState();
+        System.out.printf("\nCreated the thread %s", t1.getState());
+        t1.start();
+        t1.join();
+        System.out.printf("\nThread finished %s", t1.getState());
+
+
+    }
+}
